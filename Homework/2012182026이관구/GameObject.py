0fe274c5f4c1bb36e4b9bd3155f1cf7ab7ecd643
dufdif <klkb1@naver.com>
2017-10-14 13:01:26 +0900

    Homework - 챕터7 구현완료
    f1~f5로 플레이어 선택 키보드로이동

--- Homework/2012182026이관구/GameObject.py
+++ Homework/2012182026이관구/GameObject.py
@@ -1,6 +1,6 @@
 from pico2d import *
 import random
-
+import main_state
 #   ----    모든 게임 오브젝트를 여기서 설계  ----
 
 class Grass:  # 풀은 간단하게 그리기 기능만 가지며 이미지를 가지고 있음
@@ -20,6 +20,8 @@ class boy:  # 소년은 위치와 이미지, 프레임을 가진다.
     RightRun=1
     LeftStand=2
     RightStand=3
+    duration=100
+
     def __init__(self,i):
         self.x = random.randint(100, 700)
         self.y = random.randint(90, 200)
@@ -37,7 +39,8 @@ class boy:  # 소년은 위치와 이미지, 프레임을 가진다.
         if self.x<0:
             self.x=0
             self.state=self.RightRun
-        if self.runframe==100:
+        if self.runframe==self.duration:
+            self.duration=random.randint(80,120)
             self.state=self.LeftStand
             self.standframe=0
     def handleLeftStand(self):
@@ -46,13 +49,15 @@ class boy:  # 소년은 위치와 이미지, 프레임을 가진다.
             self.runframe=0
             self.state=self.LeftRun
 
+
     def handleRightRun(self):
         self.x += 5
         self.runframe += 1
         if self.x > 800:
             self.x = 800
             self.state = self.LeftRun
-        if self.runframe == 100:
+        if self.runframe == self.duration:
+            self.duration = random.randint(80, 120)
             self.state = self.RightStand
             self.standframe = 0
 
@@ -66,7 +71,8 @@ class boy:  # 소년은 위치와 이미지, 프레임을 가진다.
 
     def update(self):  # 업데이트 함수는 그리기와 매 프레임마다 처리해야할 행동(x축으로 2만큼 매프레임마다 이동)을 처리
         self.frame = (self.frame + 1) % 8
-        self.handlestate[self.state](self)
+        if self != main_state.player:
+            self.handlestate[self.state](self)
 
 
     def Draw(self): # 그리기 함수
